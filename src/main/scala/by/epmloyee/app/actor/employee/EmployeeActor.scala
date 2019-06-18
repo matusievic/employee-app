0fbb794d430c2ac883ead81a02d7330f1e5fbc90
Andrei Matusevich <amatusevich@mobileiron.com>
2019-06-18 15:51:16 +0300

    Add phone number class

--- src/main/scala/by/epmloyee/app/actor/employee/EmployeeActor.scala
+++ src/main/scala/by/epmloyee/app/actor/employee/EmployeeActor.scala
@@ -8,25 +8,25 @@ class EmployeeActor extends PersistentActor with ActorLogging {
   var state = State(1L, "First", "First", List(), 1, 5)
 
   override def receiveRecover: Receive = {
-    case PhoneAddedEvent(number, snapshotTimer) =>
-      state = state.addPhone(number).snapshotTimer(snapshotTimer)
-    case PhoneUpdatedEvent(index, number, snapshotTimer) =>
-      state = state.updatePhone(index, number).snapshotTimer(snapshotTimer)
-    case PhoneDeletedEvent(number, snapshotTimer) =>
-      state = state.deletePhone(number).snapshotTimer(snapshotTimer)
+    case PhoneAddedEvent(phone, snapshotTimer) =>
+      state = state.addPhone(phone).snapshotTimer(snapshotTimer)
+    case PhoneUpdatedEvent(index, phone, snapshotTimer) =>
+      state = state.updatePhone(index, phone).snapshotTimer(snapshotTimer)
+    case PhoneDeletedEvent(phone, snapshotTimer) =>
+      state = state.deletePhone(phone).snapshotTimer(snapshotTimer)
     case SnapshotOffer(_, s: State) =>
       state = s
   }
 
   override def receiveCommand: Receive = {
-    case AddPhoneRequest(number) =>
-      state.phones.find(_ == number) match {
+    case AddPhoneRequest(phone) =>
+      state.phones.find(_ == phone) match {
         case Some(_) =>
           sender ! AddPhoneResponse(None)
         case None =>
-          state = state.addPhone(number)
-          persist(PhoneAddedEvent(number, state.snapshotTimer))
-          sender ! AddPhoneResponse(Some(number))
+          state = state.addPhone(phone)
+          persist(PhoneAddedEvent(phone, state.snapshotTimer))
+          sender ! AddPhoneResponse(Some(phone))
       }
 
     case ReadAllPhonesRequest() =>
@@ -35,22 +35,22 @@ class EmployeeActor extends PersistentActor with ActorLogging {
     case ReadPhoneRequest(index) =>
       sender ! ReadPhoneResponse(state.phones.lift(index))
 
-    case UpdatePhoneRequest(index, number) =>
+    case UpdatePhoneRequest(index, phone) =>
       state.phones.lift(index) match {
         case Some(_) =>
-          state = state.updatePhone(index, number)
-          persist(PhoneUpdatedEvent(index, number, state.snapshotTimer))
-          sender ! UpdatePhoneResponse(Some(number))
+          state = state.updatePhone(index, phone)
+          persist(PhoneUpdatedEvent(index, phone, state.snapshotTimer))
+          sender ! UpdatePhoneResponse(Some(phone))
         case None =>
           sender ! UpdatePhoneResponse(None)
       }
 
     case DeletePhoneRequest(index) =>
       state.phones.lift(index) match {
-        case Some(number) =>
-          state = state.deletePhone(number)
-          persist(PhoneDeletedEvent(number, state.snapshotTimer))
-          sender ! DeletePhoneResponse(Some(number))
+        case Some(phone) =>
+          state = state.deletePhone(phone)
+          persist(PhoneDeletedEvent(phone, state.snapshotTimer))
+          sender ! DeletePhoneResponse(Some(phone))
         case None =>
           sender ! DeletePhoneResponse(None)
       }
@@ -74,37 +74,39 @@ object EmployeeActor {
   val props = Props(classOf[EmployeeActor])
 
 
-  case class State(id: Long, name: String, surname: String, phones: List[String], salary: Double, snapshotTimer: Int) {
-    def addPhone(number: String): State = copy(phones = phones :+ number)
-    def updatePhone(index: Int, number: String): State = copy(phones = phones.updated(index, number))
-    def deletePhone(number: String): State = copy(phones = phones.filterNot(_ == number))
+  case class State(id: Long, name: String, surname: String, phones: List[Phone], salary: Double, snapshotTimer: Int) {
+    def addPhone(phone: Phone): State = copy(phones = phones :+ phone)
+    def updatePhone(index: Int, number: Phone): State = copy(phones = phones.updated(index, number))
+    def deletePhone(phone: Phone): State = copy(phones = phones.filterNot(_ == phone))
     def decrementSnapshotTimer(): State = copy(snapshotTimer = snapshotTimer - 1)
     def resetSnapshotTimer(): State = copy(snapshotTimer = 5)
     def snapshotTimer(snapshotTimer: Int): State = copy(snapshotTimer = snapshotTimer)
   }
 
+  case class Phone(code: String, number: String)
+
 
   sealed trait EmployeeMessage
 
-  case class AddPhoneRequest(number: String) extends EmployeeMessage
-  case class AddPhoneResponse(result: Option[String]) extends EmployeeMessage
+  case class AddPhoneRequest(phone: Phone) extends EmployeeMessage
+  case class AddPhoneResponse(result: Option[Phone]) extends EmployeeMessage
 
   case class ReadAllPhonesRequest() extends EmployeeMessage
-  case class ReadAllPhonesResponse(result: Seq[String]) extends EmployeeMessage
+  case class ReadAllPhonesResponse(result: Seq[Phone]) extends EmployeeMessage
 
   case class ReadPhoneRequest(index: Int) extends EmployeeMessage
-  case class ReadPhoneResponse(result: Option[String]) extends EmployeeMessage
+  case class ReadPhoneResponse(result: Option[Phone]) extends EmployeeMessage
 
-  case class UpdatePhoneRequest(index: Int, number: String) extends EmployeeMessage
-  case class UpdatePhoneResponse(index: Option[String]) extends EmployeeMessage
+  case class UpdatePhoneRequest(index: Int, phone: Phone) extends EmployeeMessage
+  case class UpdatePhoneResponse(index: Option[Phone]) extends EmployeeMessage
 
   case class DeletePhoneRequest(index: Int) extends EmployeeMessage
-  case class DeletePhoneResponse(result: Option[String]) extends EmployeeMessage
+  case class DeletePhoneResponse(result: Option[Phone]) extends EmployeeMessage
 
 
   sealed trait EmployeeEvent
 
-  case class PhoneAddedEvent(number: String, snapshotTimer: Int) extends EmployeeEvent
-  case class PhoneUpdatedEvent(index: Int, number: String, snapshotTimer: Int) extends EmployeeEvent
-  case class PhoneDeletedEvent(number: String, snapshotTimer: Int) extends EmployeeEvent
+  case class PhoneAddedEvent(phone: Phone, snapshotTimer: Int) extends EmployeeEvent
+  case class PhoneUpdatedEvent(index: Int, phone: Phone, snapshotTimer: Int) extends EmployeeEvent
+  case class PhoneDeletedEvent(phone: Phone, snapshotTimer: Int) extends EmployeeEvent
 }
--- src/test/scala/by/epmloyee/app/route/phone/mock/EmployeeActorMock.scala
+++ src/test/scala/by/epmloyee/app/route/phone/mock/EmployeeActorMock.scala
@@ -4,20 +4,22 @@ import akka.actor.{Actor, Props}
 import by.epmloyee.app.actor.employee.EmployeeActor._
 
 class EmployeeActorMock extends Actor {
+  val dummyPhone = Phone("1", "1")
+
   override def receive: Receive = {
-    case AddPhoneRequest(number) =>
-      sender ! AddPhoneResponse(if (number != "1") Some(number) else None)
+    case AddPhoneRequest(phone) =>
+      sender ! AddPhoneResponse(if (phone != dummyPhone) Some(phone) else None)
     case ReadAllPhonesRequest() =>
-      sender ! ReadAllPhonesResponse(Seq("1"))
+      sender ! ReadAllPhonesResponse(Seq(dummyPhone))
     case ReadPhoneRequest(index) =>
-      sender ! ReadPhoneResponse(if (index == 0) Some("1") else None)
-    case UpdatePhoneRequest(index, number) =>
-      sender ! UpdatePhoneResponse(if (index == 0) Some(number) else None)
+      sender ! ReadPhoneResponse(if (index == 0) Some(dummyPhone) else None)
+    case UpdatePhoneRequest(index, phone) =>
+      sender ! UpdatePhoneResponse(if (index == 0) Some(phone) else None)
     case DeletePhoneRequest(index) =>
-      sender ! DeletePhoneResponse(if (index == 0) Some("1") else None)
+      sender ! DeletePhoneResponse(if (index == 0) Some(dummyPhone) else None)
   }
 }
 
 object EmployeeActorMock {
-  val props: Props = Props(new EmployeeActorMock())
+  val props: Props = Props(classOf[EmployeeActorMock])
 }
