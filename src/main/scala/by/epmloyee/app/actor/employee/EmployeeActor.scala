a5a88b0c459db80a04c5725dbe9d6a9e0af51c8d
Andrei Matusevich <amatusevich@mobileiron.com>
2019-06-19 21:49:52 +0300

    Update EmployeeActor

--- src/main/scala/by/epmloyee/app/actor/employee/EmployeeActor.scala
+++ src/main/scala/by/epmloyee/app/actor/employee/EmployeeActor.scala
@@ -2,10 +2,12 @@ package by.epmloyee.app.actor.employee
 
 import akka.actor.{ActorLogging, Props}
 import akka.persistence.{PersistentActor, SnapshotOffer}
+import by.epmloyee.app.actor.common.Result
+import by.epmloyee.app.actor.common.error.{InvalidParam, NotFound}
 import by.epmloyee.app.actor.employee.EmployeeActor._
 
 class EmployeeActor extends PersistentActor with ActorLogging {
-  var state = State(1L, "First", "First", List(), 1, 5)
+  var state = State(1L, "First", "First", List.empty[Phone], 1, 5)
 
   override def receiveRecover: Receive = {
     case PhoneAddedEvent(phone, snapshotTimer) =>
@@ -19,47 +21,47 @@ class EmployeeActor extends PersistentActor with ActorLogging {
   }
 
   override def receiveCommand: Receive = {
-    case AddPhoneRequest(phone) =>
+    case AddPhone(phone) =>
       state.phones.find(_ == phone) match {
         case Some(_) =>
-          sender ! AddPhoneResponse(None)
+          sender ! Left(InvalidParam("This phone already present"))
         case None =>
           state = state.addPhone(phone)
           persist(PhoneAddedEvent(phone, state.snapshotTimer))
-          sender ! AddPhoneResponse(Some(phone))
+          sender ! Right(phone)
       }
 
-    case ReadAllPhonesRequest() =>
-      sender ! ReadAllPhonesResponse(state.phones)
+    case ReadAllPhones() =>
+      sender ! Right(state.phones)
 
-    case ReadPhoneRequest(index) =>
-      sender ! ReadPhoneResponse(state.phones.lift(index))
+    case ReadPhone(index) =>
+      sender ! state.phones.lift(index).fold(Left(InvalidParam("There's no phone with such index")) : Result[Phone])(p => Right(p))
 
-    case UpdatePhoneRequest(index, phone) =>
+    case UpdatePhone(index, phone) =>
       state.phones.lift(index) match {
         case Some(_) =>
           state = state.updatePhone(index, phone)
           persist(PhoneUpdatedEvent(index, phone, state.snapshotTimer))
-          sender ! UpdatePhoneResponse(Some(phone))
+          sender ! Right(phone)
         case None =>
-          sender ! UpdatePhoneResponse(None)
+          sender ! Left(InvalidParam("There's no phone with such index"))
       }
 
-    case DeletePhoneRequest(index) =>
+    case DeletePhone(index) =>
       state.phones.lift(index) match {
         case Some(phone) =>
           state = state.deletePhone(phone)
           persist(PhoneDeletedEvent(phone, state.snapshotTimer))
-          sender ! DeletePhoneResponse(Some(phone))
+          sender ! Right(phone)
         case None =>
-          sender ! DeletePhoneResponse(None)
+          sender ! Left(NotFound("There's no phone with such index"))
       }
   }
 
   override def persistenceId: String = self.path.name
 
   def persist(event: EmployeeEvent): Unit = {
-    super.persist(event) { e =>
+    super.persist(event) { _ =>
       context.system.eventStream.publish(event)
     }
     state = state.decrementSnapshotTimer()
@@ -88,20 +90,15 @@ object EmployeeActor {
 
   sealed trait EmployeeMessage
 
-  case class AddPhoneRequest(phone: Phone) extends EmployeeMessage
-  case class AddPhoneResponse(result: Option[Phone]) extends EmployeeMessage
+  case class AddPhone(phone: Phone) extends EmployeeMessage
 
-  case class ReadAllPhonesRequest() extends EmployeeMessage
-  case class ReadAllPhonesResponse(result: Seq[Phone]) extends EmployeeMessage
+  case class ReadAllPhones() extends EmployeeMessage
 
-  case class ReadPhoneRequest(index: Int) extends EmployeeMessage
-  case class ReadPhoneResponse(result: Option[Phone]) extends EmployeeMessage
+  case class ReadPhone(index: Int) extends EmployeeMessage
 
-  case class UpdatePhoneRequest(index: Int, phone: Phone) extends EmployeeMessage
-  case class UpdatePhoneResponse(index: Option[Phone]) extends EmployeeMessage
+  case class UpdatePhone(index: Int, phone: Phone) extends EmployeeMessage
 
-  case class DeletePhoneRequest(index: Int) extends EmployeeMessage
-  case class DeletePhoneResponse(result: Option[Phone]) extends EmployeeMessage
+  case class DeletePhone(index: Int) extends EmployeeMessage
 
 
   sealed trait EmployeeEvent
